30eb9d390526b13010ed6c190f17235b72a5ec94
wiky-avis <peta-holding@yandex.ru>
2020-12-10 13:34:12 +0300

    creating an administration site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)} # slug генерируется 
+    # автоматически из поля title с помощью атрибута prepopulated_fields
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
+
+admin.site.register(Post, PostAdmin)
