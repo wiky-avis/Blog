a05452b85e3de039d47527e0d3a5229fbdb1bfb1
wiky-avis <peta-holding@yandex.ru>
2020-12-10 18:22:46 +0300

    creating a handler for displaying an article and adding URL templates for handlers

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,4 +4,14 @@ from .models import Post
 # обработчик для отображения списка статей
 def post_list(request):
     posts = Post.published.all() # одно и то же, что и Post.objects.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts}) 
+    # функция render() для  формирования HTML-шаблона
+
+# обработчик  для  отображения  статьи
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post, slug=post, status='published', publish__year=year, 
+        publish__month=month, publish__day=day
+        ) # Если объекта не существует, будет поднята ошибка 404
+    return render(request, 'blog/post/detail.html', {'post': post})
+
