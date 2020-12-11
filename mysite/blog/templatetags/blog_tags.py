b4e9e6c0bc6431e8edc4d4a9e22942d3244af2b1
wiky-avis <peta-holding@yandex.ru>
2020-12-11 18:12:20 +0300

    creating custom template tags and filters (1)

---
+++ mysite/blog/templatetags/blog_tags.py
@@ -0,0 +1,38 @@
+# Создание собственных тегов
+
+from django import template
+from ..models import Post
+from django.db.models import Count
+
+
+register = template.Library()
+
+# выводит количество опубликованных  статей
+# Django будет использовать название функции в качестве названия тега.
+# Можно указать явно, как обращаться к тегу из шаблонов 
+# – @register.simple_tag(name='my_tag').
+@register.simple_tag  # декоратор для регистрации нового тега
+def total_posts():
+    return Post.published.count()
+
+
+# Тег для добавления последних статей блога на боковую панель.
+# Мы будем использовать инклюзивный тег, с помощью которого сможем 
+# задействовать переменные контекста, возвращаемые тегом, для формирования 
+# шаблона.
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+# Шаблонный тег для отображения статей с наибольшим количеством комментариев
+# Мы формируем QuerySet, используя метод annotate() для добавления к каждой 
+# статье количества ее комментариев. Count используется в качестве функции 
+# агрегации, которая вычисляет количество комментариев total_comments для 
+# каждого объекта Post.
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+        ).order_by('-total_comments')[:count]
