f615166cd51582d5920a742b614b159bc8ac5db7
wiky-avis <peta-holding@yandex.ru>
2020-12-13 13:39:50 +0300

    linked the Postgresql database to the project

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,8 +84,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'ENGINE': 'django.db.backends.sqlite3',
+    # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'blog',
+    'USER': 'postgres',
+    'PASSWORD': 'blog1234',
+	'HOST': '127.0.0.1',
     }
 }
 
